1cdd0856952bdef4480a185981632a7ab474d0ea
ditam <ditam@users.noreply.github.com>
2025-04-05 22:38:02 +0200

    add layering POC

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import constants from '/constants.js';
 
 function start() {
   console.log('starting');
+
+  setTimeout(function() {
+    const c1 = document.querySelector("svg #clip-1");
+
+    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('x', '300');
+    rect.setAttribute('y', '200');
+    rect.setAttribute('width', '100');
+    rect.setAttribute('height', '100');
+
+    c1.appendChild(rect);
+  }, 2000);
 }
 
 $(document).ready(function() {
